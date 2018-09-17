2d622c7fe844181cf49c68c6fa2e7228e558257e
Ammar Alammar <ammar@ammasa.net>
2018-09-17 10:26:09 +0300

    Change dev browser to Firefox

--- init.lua
+++ init.lua
@@ -227,10 +227,10 @@ function focus(appName)
   end
 end
 
-function focusChrome()
+function focusFirefox()
   local focusedWindow = hs.window.focusedWindow()
-  local chrome = hs.application.find("Google")
-  local windows = chrome and chrome:allWindows()
+  local firefox = hs.application.find("Firefox Developer Edition")
+  local windows = firefox and firefox:allWindows()
   local activeWindows = {}
 
   for _, window in pairs(windows) do
@@ -323,7 +323,7 @@ hyperBind("1", sendToMainScreen)
 hyperBind("2", sendToTv)
 
 -- Focus binding
-hyperBind("J", focusChrome)
+hyperBind("J", focusFirefox)
 hyperBind("K", function() focus("Emacs") end)
 hyperBind("L", function() focus("Terminal") end)
 hyperBind("U", function() focus("Dash") end)
